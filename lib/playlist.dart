de076e7e03754b0cae62d46d52878a409b62bc6d
muratorun <muratorun88@gmail.com>
2023-10-08 02:01:31 +0300

    PlayPauseButton OK

--- lib/playlist.dart
+++ lib/playlist.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:just_audio/just_audio.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,6 +29,8 @@ class _PlaylistPageState extends State<PlaylistPage> {
   List<Map<String, dynamic>> playlist = [];
   bool isRefreshing = false;
   bool refreshCompleted = false;
+  final AudioPlayer audioPlayer = AudioPlayer();
+  String? currentlyPlayingSong; // Çalan şarkının adını saklayacak değişken
 
   @override
   void initState() {
@@ -91,20 +94,98 @@ class _PlaylistPageState extends State<PlaylistPage> {
       appBar: AppBar(
         title: const Text('Playlist'),
       ),
-      body: RefreshIndicator(
-        onRefresh: () async {
-          await refreshList();
-        },
-        child: ListView.builder(
-          itemCount: playlist.length,
-          itemBuilder: (context, index) {
-            final song = playlist[index];
-            return ListTile(
-              title: Text(song['songName']),
+      body: Column(
+        children: [
+          Expanded(
+            child: RefreshIndicator(
+              onRefresh: () async {
+                await refreshList();
+              },
+              child: ListView.builder(
+                itemCount: playlist.length,
+                itemBuilder: (context, index) {
+                  final song = playlist[index];
+                  final isCurrentlyPlaying =
+                      song['songName'] == currentlyPlayingSong;
+
+                  return ListTile(
+                    title: Text(
+                      song['songName'],
+                      style: TextStyle(
+                        color: isCurrentlyPlaying ? Colors.blue : Colors.black,
+                        fontWeight: isCurrentlyPlaying
+                            ? FontWeight.bold
+                            : FontWeight.normal,
+                      ),
+                    ),
+                    onTap: () async {
+                      try {
+                        final audioUrl = song['songURL'] as String;
+                        await audioPlayer.setAudioSource(
+                            AudioSource.uri(Uri.parse(audioUrl)));
+                        await audioPlayer.play();
+                      } catch (error) {
+                        print('Hata oluştu: $error');
+                      }
+
+                      setState(() {
+                        currentlyPlayingSong = song['songName'];
+                      });
+                      print('Şarkı çalınıyor: ${song['songName']}');
+                    },
+                  );
+                },
+              ),
+            ),
+          ),
+          _buildPlayerControls(), // Müzik çalar kontrol panelini ekliyoruz
+        ],
+      ),
+    );
+  }
+
+  // Müzik çalar kontrol paneli
+  Widget _buildPlayerControls() {
+    return Column(
+      children: [
+        StreamBuilder<PlayerState>(
+          stream: audioPlayer.playerStateStream,
+          builder: (context, snapshot) {
+            final playerState = snapshot.data;
+            if (playerState?.playing == true) {
+              return IconButton(
+                icon: Icon(Icons.pause),
+                iconSize: 48.0,
+                onPressed: () {
+                  audioPlayer.pause();
+                },
+              );
+            } else if (playerState?.playing == false) {
+              return IconButton(
+                icon: Icon(Icons.play_arrow),
+                iconSize: 48.0,
+                onPressed: () {
+                  audioPlayer.play();
+                },
+              );
+            } else {
+              return SizedBox(); // Müzik çalmıyorsa hiçbir şey gösterme
+            }
+          },
+        ),
+        StreamBuilder<Duration?>(
+          stream: audioPlayer.durationStream,
+          builder: (context, snapshot) {
+            final duration = snapshot.data ?? Duration.zero;
+            return Text(
+              '${duration.inMinutes}:${(duration.inSeconds % 60).toString().padLeft(2, '0')}',
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
             );
           },
         ),
-      ),
+      ],
     );
   }
 }
