f9633cb1a68c7c1f7a2eb64f72e4bbc797aef9a4
muratorun <muratorun88@gmail.com>
2023-10-07 21:23:09 +0300

    11

--- lib/playlist.dart
+++ lib/playlist.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +11,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Music Player',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: PlaylistPage(),
     );
   }
@@ -22,6 +26,8 @@ class PlaylistPage extends StatefulWidget {
 
 class _PlaylistPageState extends State<PlaylistPage> {
   List<Map<String, dynamic>> playlist = [];
+  bool isRefreshing = false;
+  bool refreshCompleted = false;
 
   @override
   void initState() {
@@ -30,20 +36,53 @@ class _PlaylistPageState extends State<PlaylistPage> {
   }
 
   Future<void> fetchAndUpdatePlaylist() async {
-    // Verileri çekme işlemi
-    // ...
+    try {
+      final response = await http.get(
+        Uri.parse(
+            'https://raw.githubusercontent.com/muratorun/TurkuYolu/main/playlist.json'),
+      );
 
-    setState(() {
-      // Verileri güncelleme işlemi
-    });
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.body);
+        final updatedPlaylist =
+            List<Map<String, dynamic>>.from(jsonData['songs']);
+
+        setState(() {
+          playlist = updatedPlaylist;
+        });
+
+        print('Liste başarıyla güncellendi');
+      } else {
+        throw Exception('Failed to load playlist data');
+      }
+    } catch (error) {
+      print('Hata: $error');
+    }
   }
 
-  RefreshController _refreshController =
-      RefreshController(initialRefresh: false);
+  Future<void> refreshList() async {
+    try {
+      await fetchAndUpdatePlaylist();
 
-  void _onRefresh() async {
-    await fetchAndUpdatePlaylist();
-    _refreshController.refreshCompleted();
+      print('Swipe işlemiyle liste güncellendi');
+
+      setState(() {
+        refreshCompleted = true;
+      });
+
+      Future.delayed(Duration(seconds: 2), () {
+        setState(() {
+          refreshCompleted = false;
+        });
+      });
+    } catch (error) {
+      print('Hata: $error');
+      print('Swipe işlemiyle liste güncellenemedi');
+    } finally {
+      setState(() {
+        isRefreshing = false;
+      });
+    }
   }
 
   @override
@@ -52,18 +91,16 @@ class _PlaylistPageState extends State<PlaylistPage> {
       appBar: AppBar(
         title: Text('Playlist'),
       ),
-      body: SmartRefresher(
-        controller: _refreshController,
-        enablePullDown: true,
-        onRefresh: _onRefresh,
+      body: RefreshIndicator(
+        onRefresh: () async {
+          await refreshList();
+        },
         child: ListView.builder(
           itemCount: playlist.length,
           itemBuilder: (context, index) {
             final song = playlist[index];
             return ListTile(
               title: Text(song['songName']),
-              // Müziği çalmak için gereken işlevi burada çağırabilirsiniz.
-              // Örneğin, onPressed ile bir fonksiyonu çağırarak müziği çalabilirsiniz.
             );
           },
         ),
